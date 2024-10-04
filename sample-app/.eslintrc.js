2e3babf620097667ef1af943fe9cf7141a46eab3
Best Stephen <144573775+aihebest@users.noreply.github.com>
2024-10-04 01:53:03 +0100

    update

--- sample-app/.eslintrc.js
+++ sample-app/.eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    extends: ['airbnb-base'],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
     },
-    "rules": {
-    }
-};
+    rules: {
+    },
+  };
